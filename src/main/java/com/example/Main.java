9f11ec8491b67665a69a415bf50be99113250749
Vansh Gupta <131511941+Vansh16GUPTA@users.noreply.github.com>
2025-01-29 14:59:21 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Hello world Vansh Gupta(500107711)");
         System.out.println("Hello Sumit Sir");
         System.out.println("Hello Vansh Sir");
+         System.out.println("Hello Sumit Sir");
+        
     }
 }
